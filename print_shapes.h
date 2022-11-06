68b57d173e09ddc47df03442eac72938a563f8cc
islamux <fathi733@gmail.com>
2022-11-06 08:03:28 +0300

    add decoration func

---
+++ print_shapes.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void PrintShapes(int numberOfShapes, char shape){
+    
+    cout << "\n";
+    for(int i = 0; i <= numberOfShapes; i++){
+        cout << shape;
+    }
+    cout << "\n";
+
+}
